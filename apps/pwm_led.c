cd8ddd2ddec917a1d45cd3b33a18a61cd0b763b5
Joamerson Islan <joamersonislan38@gmail.com>
2025-02-09 17:31:46 -0300

    refactor: refactor pwm led app

--- apps/pwm_led.c
+++ apps/pwm_led.c
@@ -7,40 +7,33 @@
 const uint8_t LED_PULSE_SPEED = 50; // The higher the number, the lower the speed
 uint16_t LED_START_LEVEL = 100;     // Nível inicial do PWM (duty cycle)
 
+// Configures the PWM function on the LED pin
 void setup_pwm() {
   uint slice;
-  gpio_set_function(LED, GPIO_FUNC_PWM);    // Configura o pino do LED para função PWM
-  slice = pwm_gpio_to_slice_num(LED);       // Obtém o slice do PWM associado ao pino do LED
-  pwm_set_clkdiv(slice, DIVIDER_PWM);       // Define o divisor de clock do PWM
-  pwm_set_wrap(slice, LED_PWM_PERIOD);      // Configura o valor máximo do contador (período do PWM)
-  pwm_set_gpio_level(LED, LED_START_LEVEL); // Define o nível inicial do PWM para o pino do LED
-  pwm_set_enabled(slice, true);             // Habilita o PWM no slice correspondente
+  gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+  slice = pwm_gpio_to_slice_num(LED_PIN);
+  pwm_set_clkdiv(slice, DIVIDER_PWM);
+  pwm_set_wrap(slice, LED_PWM_PERIOD);
+  pwm_set_gpio_level(LED_PIN, LED_START_LEVEL);
+  pwm_set_enabled(slice, true);
 }
 
-int run_pwm_led() {
-  uint up_down = 1; // Variável para controlar se o nível do LED aumenta ou diminui
-  setup_pwm();      // Configura o PWM
+void run_pwm_led() {
+  bool go_up = true;
+  setup_pwm();
   while (true) {
     if (gpio_get(SW_PIN) == 1) {
-      pwm_set_gpio_level(LED, LED_START_LEVEL); // Define o nível atual do PWM (duty cycle)
+      pwm_set_gpio_level(LED_PIN, LED_START_LEVEL); // Updates PWM level
       for (int i = 0; i < LED_PULSE_SPEED; i++) {
-        if (gpio_get(SW_PIN) == 1) {
+        if (gpio_get(SW_PIN) == 1) { // Button is not pressed?
           sleep_ms(1);
         }
       }
-      if (up_down) {
-        LED_START_LEVEL += LED_STEP; // Incrementa o nível do LED
-        if (LED_START_LEVEL >= LED_PWM_PERIOD)
-          up_down = 0; // Muda direção para diminuir quando atingir o período máximo
-      } else {
-        LED_START_LEVEL -= LED_STEP; // Decrementa o nível do LED
-        if (LED_START_LEVEL <= 0)
-          up_down = 1; // Muda direção para aumentar quando atingir o nível mínimo
-      }
+      LED_START_LEVEL += (go_up) ? +LED_DUTY_STEP : -LED_DUTY_STEP;                                              // Updates duty cycle level
+      go_up = (go_up && LED_START_LEVEL >= LED_PWM_PERIOD) || (!go_up && LED_START_LEVEL <= 0) ? !go_up : go_up; // Controls if duty cycle will increase or decrease
     } else {
-      pwm_set_gpio_level(LED, 0);
+      pwm_set_gpio_level(LED_PIN, 0);
       break;
     }
   }
-  return 0;
 }
--- apps/pwm_led.h
+++ apps/pwm_led.h
@@ -1,13 +1,13 @@
-#include "pico/stdlib.h" // Biblioteca padrão do Raspberry Pi Pico
-#include <stdio.h>       // Biblioteca padrão de entrada e saída
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 #define LED_R_PIN 13
 #define LED_G_PIN 11
 #define LED_B_PIN 12
 
-static const uint LED = 12;                  // Pino do LED conectado
-static const uint16_t LED_PWM_PERIOD = 2000; // Período do PWM (valor máximo do contador)
+static const uint8_t LED_PIN = 12;
+static const uint16_t LED_PWM_PERIOD = 2000; // PWM period (max value)
 static const float DIVIDER_PWM = 16.0;       // Divisor fracional do clock para o PWM
-static const uint16_t LED_STEP = 100;        // Passo de incremento/decremento para o duty cycle do LED
+static const int16_t LED_DUTY_STEP = 100;
 
-extern int run_pwm_led();
+extern void run_pwm_led();
