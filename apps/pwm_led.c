fda5b1a350618fd12bbd6d8c1bb7154b7e868c40
Joamerson Islan <joamersonislan38@gmail.com>
2025-02-08 21:18:48 -0300

    feat: continue implementation of menu with pwd led app

--- apps/pwm_led.c
+++ apps/pwm_led.c
@@ -1,8 +1,8 @@
 #include "pwm_led.h"
 #include "hardware/pwm.h"
+#include "joystick_led.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 uint16_t led_level = 100; // Nível inicial do PWM (duty cycle)
 
 void setup_pwm() {
@@ -17,19 +17,28 @@ void setup_pwm() {
 
 int run_pwm_led() {
   uint up_down = 1; // Variável para controlar se o nível do LED aumenta ou diminui
-  stdio_init_all(); // Inicializa o sistema padrão de I/O
-  setup_pwm();      // Configura o PWM
+  // stdio_init_all(); // Inicializa o sistema padrão de I/O
+  setup_pwm(); // Configura o PWM
   while (true) {
-    pwm_set_gpio_level(LED, led_level); // Define o nível atual do PWM (duty cycle)
-    sleep_ms(1000);                     // Atraso de 1 segundo
-    if (up_down) {
-      led_level += LED_STEP; // Incrementa o nível do LED
-      if (led_level >= LED_PWM_PERIOD)
-        up_down = 0; // Muda direção para diminuir quando atingir o período máximo
+    if (gpio_get(SW_PIN) == 1) {
+      pwm_set_gpio_level(LED, led_level); // Define o nível atual do PWM (duty cycle)
+      for (int i = 0; i < 1000; i++) {
+        if (gpio_get(SW_PIN) == 1) {
+          sleep_ms(1);
+        }
+      }
+      if (up_down) {
+        led_level += LED_STEP; // Incrementa o nível do LED
+        if (led_level >= LED_PWM_PERIOD)
+          up_down = 0; // Muda direção para diminuir quando atingir o período máximo
+      } else {
+        led_level -= LED_STEP; // Decrementa o nível do LED
+        if (led_level <= 0)
+          up_down = 1; // Muda direção para aumentar quando atingir o nível mínimo
+      }
     } else {
-      led_level -= LED_STEP; // Decrementa o nível do LED
-      if (led_level <= 0)
-        up_down = 1; // Muda direção para aumentar quando atingir o nível mínimo
+      pwm_set_gpio_level(LED, 0);
+      break;
     }
   }
   return 0;
